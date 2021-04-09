2c6cc687ebef4c99ea9554903eacca9e57325534
Maxwell <maxwell.wong@cadcam.com.my>
2021-04-09 15:19:09 +0800

    fix server path

--- src/doc/variables.js
+++ src/doc/variables.js
@@ -1,4 +1,4 @@
-//const serverPath='https://imelibrary.herokuapp.com'; // actual server
-const serverPath='http://localhost:5000'; // testing server
+const serverPath='https://imelibrary.herokuapp.com'; // actual server
+//const serverPath='http://localhost:5000'; // testing server
 
 module.exports=serverPath;
